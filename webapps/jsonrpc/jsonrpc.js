93ca4b3a152fd22ebc9d1b0359b1457b5caf4ce7
William Becker <wbecker@gmail.com>
2007-10-09 05:20:20 +0000

    Took out the createObject function. I won't put that in till after 1.2!

--- webapps/jsonrpc/jsonrpc.js
+++ webapps/jsonrpc/jsonrpc.js
@@ -514,46 +514,6 @@ JSONRpcClient.prototype._createMethod = function (methodName)
   return serverMethodCaller;
 };
 
-/**
- * Creates a new object from the bridge. A callback may optionally be given as
- * the first argument to make this an async call.
- *
- * @param callback (optional)
- * @param constructorName The name of the class to create, which should be 
- *   registered with JSONRPCBridge.registerClass()
- * @param _args The arguments the constructor takes
- * @return the new object if sync, the request id if async.
- */
-JSONRpcClient.prototype.createObject = function ()
-{
-  var args = [],
-      callback = null,
-      constructorName,
-      _args,
-      req;
-  for(var i=0;i<arguments.length;i++)
-  {
-    args.push(arguments[i]);
-  }
-  if(typeof args[0] == "function")
-  {
-    callback = args.shift();
-  }
-  constructorName=args[0]+".constructor";
-  _args=args[1];      
-        
-  req = this._makeRequest.call(this, constructorName, _args, callback);
-  if(callback === null) 
-  {
-    return this._sendRequest.call(this, req);
-  }
-  else 
-  {
-    JSONRpcClient.async_requests.push(req);
-    JSONRpcClient.kick_async();
-    return req.requestId;
-  }
-};
 
 /**
  * This is used to add a list of methods to this.
