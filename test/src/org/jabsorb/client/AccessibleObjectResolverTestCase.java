d92262e0b07f2dc83c526484ebf0e4bba7c8b7d9
Arthur Blake <arthur.blake@gmail.com>
2008-01-17 16:11:49 +0000

    remove unused import (unused in the code and unavailable in JDK 1.4) to fix JDK 1.4 compilation issue

--- test/src/org/jabsorb/client/AccessibleObjectResolverTestCase.java
+++ test/src/org/jabsorb/client/AccessibleObjectResolverTestCase.java
@@ -2,7 +2,6 @@ package org.jabsorb.client;
 
 import java.lang.reflect.AccessibleObject;
 import java.lang.reflect.Constructor;
-import java.lang.reflect.Type;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,26 +15,26 @@ import org.json.JSONArray;
 import junit.framework.TestCase;
 
 public class AccessibleObjectResolverTestCase extends TestCase {
-	AccessibleObjectResolver resolver;
-	Map methodMap;
-	JSONSerializer serializer;
-	
-	protected void setUp() throws Exception {
-		resolver= new AccessibleObjectResolver();
-		methodMap = new HashMap();
+  AccessibleObjectResolver resolver;
+  Map methodMap;
+  JSONSerializer serializer;
+
+  protected void setUp() throws Exception {
+    resolver= new AccessibleObjectResolver();
+    methodMap = new HashMap();
         methodMap.putAll(ClassAnalyzer.getClassData(ConstructorTest.class).getMethodMap());
         methodMap.putAll(ClassAnalyzer.getClassData(ConstructorTest.class).getConstructorMap());
         serializer = new JSONSerializer();
         serializer.registerDefaultSerializers();
-	}
+  }
 
-	public void testResolution() {
-		JSONArray args= new JSONArray();
-		args.put(1);
-		Constructor methodInt= (Constructor)resolver.resolveMethod(methodMap, "$constructor", args, serializer);
-		Class[] params= methodInt.getParameterTypes();
-		assertNotNull(params);
-		assertEquals(1, params.length);
-		assertEquals(Integer.TYPE, params[0]);
-	}
+  public void testResolution() {
+    JSONArray args= new JSONArray();
+    args.put(1);
+    Constructor methodInt= (Constructor)resolver.resolveMethod(methodMap, "$constructor", args, serializer);
+    Class[] params= methodInt.getParameterTypes();
+    assertNotNull(params);
+    assertEquals(1, params.length);
+    assertEquals(Integer.TYPE, params[0]);
+  }
 }
