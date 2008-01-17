fda96c9f6d429c262bc417b41c1a6c1a84253800
Sasha Ovsankin <sasha.ov@gmail.com>
2008-01-17 07:00:48 +0000

    Test for AccessibleObjectResolver -- currently fails in Jabsorb unit tests on my computer

---
+++ test/src/org/jabsorb/client/AccessibleObjectResolverTest.java
@@ -0,0 +1,41 @@
+package org.jabsorb.client;
+
+import java.lang.reflect.AccessibleObject;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Type;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.jabsorb.JSONSerializer;
+import org.jabsorb.reflect.ClassAnalyzer;
+import org.jabsorb.serializer.AccessibleObjectResolver;
+import org.jabsorb.serializer.Serializer;
+import org.jabsorb.test.ConstructorTest;
+import org.json.JSONArray;
+
+import junit.framework.TestCase;
+
+public class AccessibleObjectResolverTest extends TestCase {
+	AccessibleObjectResolver resolver;
+	Map methodMap;
+	JSONSerializer serializer;
+	
+	protected void setUp() throws Exception {
+		resolver= new AccessibleObjectResolver();
+		methodMap = new HashMap();
+        methodMap.putAll(ClassAnalyzer.getClassData(ConstructorTest.class).getMethodMap());
+        methodMap.putAll(ClassAnalyzer.getClassData(ConstructorTest.class).getConstructorMap());
+        serializer = new JSONSerializer();
+        serializer.registerDefaultSerializers();
+	}
+
+	public void testResolution() {
+		JSONArray args= new JSONArray();
+		args.put(1);
+		Constructor methodInt= (Constructor)resolver.resolveMethod(methodMap, "$constructor", args, serializer);
+		Class[] params= methodInt.getParameterTypes();
+		assertNotNull(params);
+		assertEquals(1, params.length);
+		assertEquals(Integer.TYPE, params[0]);
+	}
+}
