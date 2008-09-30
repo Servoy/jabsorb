1cf23680dd4662c646e69f9f7d50c0e93ecaeaa8
William Becker <wbecker@gmail.com>
2008-09-30 14:37:12 +0000

    Commented

--- src/org/jabsorb/serializer/request/DefaultRequestParser.java
+++ src/org/jabsorb/serializer/request/DefaultRequestParser.java
@@ -5,6 +5,10 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+/**
+ * A simple request parser that just returns the argument array without doing
+ * anything
+ */
 public class DefaultRequestParser implements RequestParser
 {
   public JSONArray unmarshallArguments(JSONObject jsonReq) throws JSONException
