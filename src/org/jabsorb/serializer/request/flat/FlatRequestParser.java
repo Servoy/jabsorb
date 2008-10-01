c553210588581fa9288800e3e9a93c7450ae30a5
William Becker <wbecker@gmail.com>
2008-10-01 18:05:35 +0000

    Adding FlatSerializer, which can be used in place of the Fixups serializer to serialize circ refs/duplicates.

---
+++ src/org/jabsorb/serializer/request/flat/FlatRequestParser.java
@@ -0,0 +1,136 @@
+package org.jabsorb.serializer.request.flat;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.Map.Entry;
+
+import org.jabsorb.JSONSerializer;
+import org.jabsorb.serializer.request.RequestParser;
+import org.jabsorb.serializer.response.flat.FlatSerializerState;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Reads a "flattened" json message, in which all objects exist as seperate keys
+ * on the main object, to allow circular references, eg:
+ * {"result":"_1","_1":{"circRef":"_1"}}.
+ * 
+ * @author William Becker
+ */
+public class FlatRequestParser implements RequestParser
+{
+  /**
+   * Parses an array
+   * 
+   * @author William Becker
+   */
+  private class ArrayParser
+  {
+    /**
+     * The indexes of objects that have been read already mapped to the objects
+     */
+    private final Map<String, JSONObject> parsedObjects;
+
+    /**
+     * Creates a new ArrayParser
+     */
+    public ArrayParser()
+    {
+      parsedObjects = new HashMap<String, JSONObject>();
+    }
+
+    /**
+     * Reads an array with (potentially) indexes which point to objects and returns
+     * a properly constructed array;
+     * 
+     * @param array The array to parse
+     * @param jsonReq The main object that contains indexes mapped to objects
+     * @return An array that has all objects constructed
+     * @throws JSONException If the json is improperly constructed
+     */
+    public JSONArray parseArray(JSONArray array, JSONObject jsonReq)
+        throws JSONException
+    {
+      for (int i = 0; i < array.length(); i++)
+      {
+        Object o = array.get(i);
+        if (isObjectIndex(o))
+        {
+          array.put(i, getObject((String) o, jsonReq));
+        }
+        else if (o instanceof JSONArray)
+        {
+          array.put(i, parseArray((JSONArray) o, jsonReq));
+        }
+      }
+      return array;
+    }
+
+    /**
+     * Gets an object by its index
+     * 
+     * @param index The index of the object
+     * @param jsonReq The object which maps indexes to objects
+     * @return The object requested
+     * @throws JSONException If the json cannot be read
+     */
+    private JSONObject getObject(String index, JSONObject jsonReq)
+        throws JSONException
+    {
+      if (this.parsedObjects.containsKey(index))
+      {
+        return this.parsedObjects.get(index);
+      }
+      JSONObject o = jsonReq.getJSONObject(index);
+      this.parsedObjects.put(index, o);
+      Map<String, Object> newObjects = new TreeMap<String, Object>();
+      for (Iterator<?> i = o.keys(); i.hasNext();)
+      {
+        String k = (String) i.next();
+        Object v = o.get(k);
+        if (isObjectIndex(v))
+        {
+          Object ob = getObject((String) v, jsonReq);
+          newObjects.put(k, ob);
+        }
+        else if (v instanceof JSONArray)
+        {
+          newObjects.put(k, parseArray((JSONArray) v, jsonReq));
+        }
+      }
+      for (Entry<String, Object> e : newObjects.entrySet())
+      {
+        o.put(e.getKey(), e.getValue());
+      }
+      return o;
+    }
+
+    /**
+     * Is the given object an index which is mapped to an object?
+     * 
+     * @param o The object to test
+     * @return Whether the object is an index
+     */
+    private boolean isObjectIndex(Object o)
+    {
+      if (o instanceof String)
+      {
+        String s = (String) o;
+        if (s.startsWith(FlatSerializerState.INDEX_PREFIX))
+        {
+          return true;
+        }
+      }
+      return false;
+    }
+  }
+
+  public JSONArray unmarshallArguments(JSONObject jsonReq) throws JSONException
+  {
+    return new ArrayParser().parseArray(jsonReq
+        .getJSONArray(JSONSerializer.PARAMETER_FIELD), jsonReq);
+  }
+}
---
+++ src/org/jabsorb/serializer/response/flat/FlatSerializerState.java
@@ -0,0 +1,175 @@
+package org.jabsorb.serializer.response.flat;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.jabsorb.serializer.MarshallException;
+import org.jabsorb.serializer.ProcessedObject;
+import org.jabsorb.serializer.SerializerState;
+import org.jabsorb.serializer.UnmarshallException;
+import org.jabsorb.serializer.response.results.SuccessfulResult;
+import org.json.JSONObject;
+
+/**
+ * Serializes objects into a flat style. This means that all JSONObjects exist
+ * at the top level. This prevents duplication and circular references from
+ * occuring.
+ * 
+ * @author William Becker
+ */
+public class FlatSerializerState implements SerializerState
+{
+  /**
+   * The start indentifier of an index
+   */
+  public static final String INDEX_PREFIX = "_";
+
+  /**
+   * The current index value
+   */
+  private int index;
+
+  /**
+   * All values which are known to be marshalled to JSONObjects
+   */
+  private final Map<Integer, FlatProcessedObject> marshalledObjects;
+
+  /**
+   * All values which either aren't to be marshalled to JSONObjects or it is
+   * unknown to what they will be unmarshalled
+   */
+  private final Map<Integer, FlatProcessedObject> nonMarshalledObjects;
+
+  /**
+   * Creates a new FlatSerializerState
+   */
+  public FlatSerializerState()
+  {
+    this.marshalledObjects = new HashMap<Integer, FlatProcessedObject>();
+    this.nonMarshalledObjects = new HashMap<Integer, FlatProcessedObject>();
+    this.index = 1;
+  }
+
+  public Object checkObject(Object parent, Object currentObject, Object ref)
+      throws MarshallException
+  {
+    FlatProcessedObject o = getProcessedObject(currentObject);
+    if (o != null)
+    {
+      return o.getSerialized();
+    }
+    push(null, currentObject, null);
+    return null;
+  }
+
+  public SuccessfulResult createResult(Object requestId, Object object,
+      Object json)
+  {
+    return new FlatResult(requestId, json, this.marshalledObjects);
+  }
+
+  /**
+   * Get the JSONObject for the given marshalled object
+   * 
+   * @param o The java object
+   * @return A JSON object for the java object.
+   */
+  public JSONObject getJSONObject(Object o)
+  {
+    JSONObject j = (JSONObject) marshalledObjects.get(
+        new Integer(System.identityHashCode(o))).getObject();
+    return j;
+  }
+
+  public FlatProcessedObject getProcessedObject(Object object)
+  {
+    final int key = System.identityHashCode(object);
+    if (this.marshalledObjects.containsKey(key))
+    {
+      return this.marshalledObjects.get(key);
+    }
+    return this.nonMarshalledObjects.get(key);
+  }
+
+  public void pop() throws MarshallException
+  {
+    //Nothing to do
+  }
+
+  public Object push(Object parent, Object obj, Object ref)
+  {
+    final int identity = new Integer(System.identityHashCode(obj));
+    final Object toReturn;
+    final FlatProcessedObject po;
+    if (obj instanceof JSONObject)
+    {
+      JSONObject val = new JSONObject();
+      String _index = nextIndex();
+      po = new FlatProcessedObject(val, _index);
+
+      toReturn = _index;
+      if (!this.marshalledObjects.containsKey(identity))
+      {
+        this.marshalledObjects.put(identity, po);
+      }
+    }
+    else
+    {
+      po = new FlatProcessedObject(obj);
+      toReturn = obj;
+      if (!this.nonMarshalledObjects.containsKey(identity))
+      {
+        this.nonMarshalledObjects.put(identity, po);
+      }
+    }
+    return toReturn;
+    //throw new MarshallException("Object already marshalled.");
+  }
+
+  public void setMarshalled(Object marshalledObject, Object java)
+  {
+    if (marshalledObject instanceof JSONObject)
+    {
+      FlatProcessedObject o = getProcessedObject(java);
+      o.setIndexValue(nextIndex());
+      final int key = System.identityHashCode(java);
+      if (this.nonMarshalledObjects.containsKey(key))
+      {
+        this.marshalledObjects.put(key, this.nonMarshalledObjects.remove(key));
+      }
+    }
+  }
+
+  public void setSerialized(Object source, Object target)
+      throws UnmarshallException
+  {
+    final ProcessedObject po = this.getProcessedObject(source);
+    if (po != null)
+    {
+      po.setSerialized(target);
+    }
+  }
+
+  public void store(Object object)
+  {
+    if (object instanceof JSONObject)
+    {
+      FlatProcessedObject p = new FlatProcessedObject(object, nextIndex());
+      final int identity = System.identityHashCode(object);
+      if (!marshalledObjects.containsKey(identity))
+      {
+        marshalledObjects.put(identity, p);
+      }
+    }
+  }
+
+  /**
+   * Creates the next index value
+   * 
+   * @return The next index value
+   */
+  private String nextIndex()
+  {
+    return FlatSerializerState.INDEX_PREFIX + index++;
+  }
+}
